98e021c22f6e04ea9a2df734f165b329d144ac1b
Rob <rsamoraj11@gmail.com>
2024-05-29 20:53:24 -0500

    Add interactive cli to guidedAttackTree

--- spiderweb/src/spiderweb/guidedAttackTree.py
+++ spiderweb/src/spiderweb/guidedAttackTree.py
@@ -16,14 +16,31 @@ EXAMPLES
 #TODO: write examples
 
 """
+import cmd
+from pprint import pprint
+import requests
+import shlex
 
-def listSteps() -> None:
-    print("\tDid you... ")
-    print("\t\tcheck the source code?")
-    print("\t\tcheck the comments?")
-    print("\t\tlook at robots.txt?")
+class GuidedCLI(cmd.Cmd):
+    intro = 'Web attack interactive guide'
+    prompt = 'spiderweb> '
+
+    def __init__(self, *args, **kwargs):
+        super(GuidedCLI, self).__init__(*args, **kwargs)
+        self.targetUrl = None
+
+    def do_get  (self, url):
+        r = requests.get(url)
+        pprint(f"Head for: {url}\n {r.headers}")
+
 
 
 if __name__ == "__main__":
-    listSteps()
+    GuidedCLI().cmdloop()
+    # listSteps()
 
+# def listSteps() -> None:
+#     print("\tDid you... ")
+#     print("\t\tcheck the source code?")
+#     print("\t\tcheck the comments?")
+#     print("\t\tlook at robots.txt?")
